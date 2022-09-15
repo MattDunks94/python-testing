51d145481ce79b135c64f2cac33b293381c053ca
Matt <mjd014@live.co.uk>
2022-09-15 16:16:32 +0000

    added new function to our TestStudent class, checks to see if student is on santas naughty_list.

--- test_student.py
+++ test_student.py
@@ -4,11 +4,17 @@ from student import Student
 class TestStudent(unittest.TestCase):
 
     def test_full_name(self):
-
         student = Student('John', 'Doe')
 
         self.assertEqual(student.full_name, 'John Doe')
     
-    if __name__ == '__main__':
-        unittest.main()
 
+    def test_alert_santa(self):
+        student = Student('John', 'Doe')
+        student.alert_santa()
+
+        self.assertTrue(student.naughty_list)
+
+
+if __name__ == '__main__':
+    unittest.main()
