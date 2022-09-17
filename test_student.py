57244d4a9dbc5b2f3acc0cf5d1c1ed780d9b796e
Matt <mjd014@live.co.uk>
2022-09-17 11:28:58 +0000

    added two classmethods, one that operates before testing and one for after.

--- test_student.py
+++ test_student.py
@@ -7,23 +7,40 @@ from student import Student
 # Tests for our 'Student' class
 class TestStudent(unittest.TestCase):
 
+    # @classmethod is a method that is bound to a class rather than its object. 
+    # Is always attached to the class when passed 'cls'.
+    # This runs before anything else.
+    @classmethod
+    def setUpClass(cls):
+        print('setUpClass')
+
+    # This runs at the end of testing.
+    @classmethod    
+    def tearDownClass(cls):
+        print('tearDownClass')
+
+    def setUp(self):
+        print('setUp')
+        self.student = Student('John', 'Doe')
+
+    def tearDown(self):
+        print('tearDown')
+
     # Testing to see if full_name is equal to 'John Doe'
     def test_full_name(self):
-        student = Student('John', 'Doe')
-
-        self.assertEqual(student.full_name, 'John Doe')
+        print('test_full_name')
+        self.assertEqual(self.student.full_name, 'John Doe')
     
     # Testing to see if our student is on the naughty_list
     def test_alert_santa(self):
-        student = Student('John', 'Doe')
-        student.alert_santa()
+        print('test_alert_santa')
+        self.student.alert_santa()
 
-        self.assertTrue(student.naughty_list)
+        self.assertTrue(self.student.naughty_list)
 
     def test_email(self):
-        student = Student('John', 'Doe')
-
-        self.assertEqual(student.email, 'john.doe@email.com')
+        print('test_email')
+        self.assertEqual(self.student.email, 'john.doe@email.com')
 
 
 if __name__ == '__main__':
